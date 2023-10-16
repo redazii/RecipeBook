9096fcfce4734feb71c8858aecae0e5f8fbb6564
redazii <147596833+redazii@users.noreply.github.com>
2023-10-16 23:18:06 +0200

    update

--- routers/router_recipes.py
+++ routers/router_recipes.py
@@ -1,45 +1,50 @@
-import uuid
 from fastapi import APIRouter, HTTPException
+from typing import List
+import uuid
 from recipes.schema_dto import Recipe, RecipeNoID
 
 router = APIRouter(
+    prefix='/recipes',
     tags=["Recipes"]
 )
 
-# Liste de recettes fictives 
 recipes = [
-    Recipe(id="recipe1", name="Spaghetti Carbonara", ingredients=["pasta", "eggs", "cheese", "pancetta"]),
-    Recipe(id="recipe2", name="Chicken Alfredo", ingredients=["chicken", "fettuccine", "cream", "Parmesan"]),
-    Recipe(id="recipe3", name="Vegetable Stir-Fry", ingredients=["vegetables", "tofu", "soy sauce", "rice"]),
+    Recipe(id="recipe1", name="Spaghetti Bolognese", ingredients=["spaghetti", "ground beef", "tomato sauce"]),
+    Recipe(id="recipe2", name="Chicken Alfredo", ingredients=["chicken", "fettuccine pasta", "alfredo sauce"]),
+    Recipe(id="recipe3", name="Vegetable Stir-Fry", ingredients=["mixed vegetables", "soy sauce", "rice"])
 ]
 
-@router.get('/recipes')
+@router.get('/', response_model=List[Recipe])
 async def get_recipes():
+    """List all the recipes."""
     return recipes
 
-@router.post('/recipes')
-async def create_recipe(recipe_data: RecipeNoID):
-    new_recipe = Recipe(**recipe_data.dict(), id=str(uuid.uuid4()))
+@router.post('/', response_model=Recipe, status_code=201)
+async def create_recipe(new_recipe: RecipeNoID):
+    generated_id = uuid.uuid4()
+    new_recipe = Recipe(id=str(generated_id), **new_recipe.dict())
     recipes.append(new_recipe)
     return new_recipe
 
-@router.get('/recipes/{recipe_id}')
+@router.get('/{recipe_id}', response_model=Recipe)
 async def get_recipe_by_id(recipe_id: str):
     for recipe in recipes:
         if recipe.id == recipe_id:
             return recipe
     raise HTTPException(status_code=404, detail="Recipe not found")
 
-@router.patch('/recipes/{recipe_id}')
-async def modify_recipe(recipe_id: str, modified_recipe_data: RecipeNoID):
+@router.patch('/{recipe_id}', status_code=204)
+async def modify_recipe_name(recipe_id: str, modified_recipe: RecipeNoID):
     for recipe in recipes:
         if recipe.id == recipe_id:
-            for key, value in modified_recipe_data.dict().items():
-                setattr(recipe, key, value)
-            return recipe
+            recipe.name = modified_recipe.name
+            return
     raise HTTPException(status_code=404, detail="Recipe not found")
 
-@router.delete('/recipes/{recipe_id}', status_code=204)
+@router.delete('/{recipe_id}', status_code=204)
 async def delete_recipe(recipe_id: str):
-    global recipes
-    recipes = [recipe for recipe in recipes if recipe.id != recipe_id]
+    for recipe in recipes:
+        if recipe.id == recipe_id:
+            recipes.remove(recipe)
+            return
+    raise HTTPException(status_code=404, detail="Recipe not found")
