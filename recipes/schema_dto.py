c8eef0c346d5bb1b974232202c912bca5b77d07c
redazii <147596833+redazii@users.noreply.github.com>
2023-10-29 20:07:18 +0100

    update

--- recipes/schema_dto.py
+++ recipes/schema_dto.py
@@ -1,31 +1,8 @@
-import datetime
 from pydantic import BaseModel
 from typing import List
 
 
-# Modèle Pydantic pour une recette
 class Recipe(BaseModel):
-    id: str
-    name: str
-    ingredients: List  # Liste d'ingrédients
-    instructions: str  # Instructions de préparation
-    cooking_time: int  # Temps de cuisson en minutes
-    created_at: datetime.datetime
-
-# Modèle Pydantic pour POST (ajouter une recette)
-class RecipeCreate(BaseModel):
-    name: str
-    ingredients: List
-    instructions: str
-    cooking_time: int
-
-# Modèle Pydantic pour la mise à jour d'une recette
-class RecipeUpdate(BaseModel):
-    name: str
-    ingredients: List
-    instructions: str
-    cooking_time: int
-
-# Modèle Pydantic pour une liste de recettes
-class RecipeList(BaseModel):
-    recipes: List[Recipe]
+  name: str
+  ingredients: List[str]
+  instructions: str
