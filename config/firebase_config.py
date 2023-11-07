985f5edbb9c4142f0529ba46d01e8eb2cf6219f3
redazii <147596833+redazii@users.noreply.github.com>
2023-11-07 21:13:39 +0100

    add authentification

---
+++ config/firebase_config.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyDvwg2L8PEqYg8Q_eBF78YoFY5hzAfGVSQ",
+  "authDomain": "myapi-8a340.firebaseapp.com",
+  "databaseURL": "https://myapi-8a340-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "myapi-8a340",
+  "storageBucket": "myapi-8a340.appspot.com",
+  "messagingSenderId": "624806681027",
+  "appId": "1:624806681027:web:e355ed184997fb1b8a0576"
+}
---
+++ database/firebase.py
@@ -0,0 +1,18 @@
+import firebase_admin
+import pyrebase
+from config.firebase_config import firebaseConfig
+
+
+#from dotenv import dotenv_values
+#import json
+#env = dotenv_values(dotenv_path='.env')
+
+if not firebase_admin._apps :
+    cred = firebase_admin.credentials.Certificate("c:/Users/redaz/Downloads/myapi-8a340-firebase-adminsdk-3uwum-0face33b2c.json")
+    firebase_admin.initialize_app(cred)
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+#authentication
+#authStudent = firebase.auth()
