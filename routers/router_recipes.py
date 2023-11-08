64d9d4c96a119765e56b6c3c99a6e8aa8c356e7d
redazii <147596833+redazii@users.noreply.github.com>
2023-11-08 14:26:09 +0100

    update

--- routers/router_recipes.py
+++ routers/router_recipes.py
@@ -22,17 +22,6 @@ def display_recipes(user_data: int= Depends(get_current_user)):
     recipes_data = db.child("recipes").get(user_data['idToken']).val()
     if recipes_data is not None:
      return [item for item in recipes_data.values()]
-
-
-# Endpoint to delete a recipe by name (DELETE)
-@router.delete("/{recipe_id}")
-def delete_recipe(recipe_id: str, user_data: int= Depends(get_current_user)):
-    recipes_data = db.child("recipes").child(recipe_id).get(user_data['idToken'])
-    if recipes_data is not None :
-     db.child("recipes").child(recipe_id).remove()
-     return {"message":"recipe deleted"}
-    else:
-      raise HTTPException(status_code=404,detail="recipe not found")
     
 
 
@@ -48,15 +37,6 @@ def add_recipe(recipe: Recipe,user_data: int= Depends(get_current_user)):
     db.child("recipes").child(recipe_id).set(recipe_data.model_dump(), token=user_data['idToken'])
     return {"message": "Recipe added successfully"}
 
-# Endpoint pour récupérer une recette par son ID (GET)
-@router.get("/{recipe_id}")
-def get_recipe_by_id(recipe_id: str, user_data: int= Depends(get_current_user)):
-    recipes_data = db.child("recipes").child(recipe_id).get(user_data['idToken'])
-    if recipes_data is not None :
-     return recipes_data.val()
-    else:
-      raise HTTPException(status_code=404,detail="recipe not found")
-      
 # Endpoint pour récupérer une recette par son nom (GET)
 @router.get("/name/{name}")
 def get_recipe_by_name(name: str ,user_data: int= Depends(get_current_user)):
@@ -67,6 +47,35 @@ def get_recipe_by_name(name: str ,user_data: int= Depends(get_current_user)):
        search_result.append(recipe)
   return search_result
 
+# Endpoint pour récupérer une recette par son nom (GET)
+@router.get("/category/{category}")
+def get_recipe_by_category(category: str ,user_data: int= Depends(get_current_user)):
+  search_result = []
+  recipes =  display_recipes(user_data=user_data)
+  for recipe in recipes:
+      if category in recipe['category'].lower():
+       search_result.append(recipe)
+  return search_result
+
+# Endpoint pour récupérer une recette par son ID (GET)
+@router.get("/{recipe_id}")
+def get_recipe_by_id(recipe_id: str, user_data: int= Depends(get_current_user)):
+    recipes_data = db.child("recipes").child(recipe_id).get(user_data['idToken'])
+    if recipes_data is not None :
+     return recipes_data.val()
+    else:
+      raise HTTPException(status_code=404,detail="recipe not found")
+    
+# Endpoint to delete a recipe by name (DELETE)
+@router.delete("/{recipe_id}")
+def delete_recipe(recipe_id: str, user_data: int= Depends(get_current_user)):
+    recipes_data = db.child("recipes").child(recipe_id).get(user_data['idToken'])
+    if recipes_data is not None :
+     db.child("recipes").child(recipe_id).remove()
+     return {"message":"recipe deleted"}
+    else:
+      raise HTTPException(status_code=404,detail="recipe not found")    
+      
 
 @router.patch("/{recipe_id}")
 def update_recipe(recipe_id: str, updated_recipe_data: dict , user_data: int= Depends(get_current_user)):
@@ -80,14 +89,3 @@ def update_recipe(recipe_id: str, updated_recipe_data: dict , user_data: int= De
         return {"message": "Recipe updated successfully"}
     else:
         raise HTTPException(status_code=404, detail="Recipe not found")
-
-
-# Endpoint pour récupérer une recette par son nom (GET)
-@router.get("/category/{category}")
-def get_recipe_by_category(category: str ,user_data: int= Depends(get_current_user)):
-  search_result = []
-  recipes =  display_recipes(user_data=user_data)
-  for recipe in recipes:
-      if category in recipe['category'].lower():
-       search_result.append(recipe)
-  return search_result
